07dccc859a25f31dfe7d5946f0d441b3a35794b7
AlexChow <cplover@live.com>
2016-05-13 18:41:56 +0800

    update

---
+++ src/VessageRESTfulServer/Controllers/ActivitiesController.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Mvc;
+using VessageRESTfulServer.Services;
+
+// For more information on enabling Web API for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace VessageRESTfulServer.Controllers
+{
+    [Route("api/[controller]")]
+    public class ActivitiesController : APIControllerBase
+    {
+        [HttpGet("BoardData")]
+        public async Task<IEnumerable<object>> Get()
+        {
+            try
+            {
+                var data = await AppServiceProvider.GetActivityService().GetActivityBoardData(UserSessionData.UserId);
+                var result = ActivityBadgeDataToJsonObject(data);
+                return result;
+            }
+            catch (Exception ex)
+            {
+#if DEBUG
+                Console.WriteLine(ex.Message);
+#endif
+                return new object[0];
+            }
+            
+
+        }
+
+        private class ActivityData
+        {
+            public string id { get; set; }
+            public int badge { get; set; }
+            public bool miniBadge { get; set; }
+        }
+
+        private IEnumerable<object> ActivityBadgeDataToJsonObject(ActivityBadgeData d)
+        {
+            var dict = new Dictionary<string, ActivityData>();
+            foreach (var badge in d.BadgeValueActivity)
+            {
+                var strs = badge.Split(':');
+                var acId = strs[0];
+                var cnt = 0;
+                try
+                {
+                    cnt = int.Parse(strs[1]);
+                    if (dict.ContainsKey(acId))
+                    {
+                        var ad = dict[acId];
+                        ad.badge += cnt;
+                    }
+                    else
+                    {
+                        dict[acId] = new ActivityData
+                        {
+                            id = acId,
+                            badge = cnt,
+                            miniBadge = false
+                        };
+                    }
+                }
+                catch (Exception)
+                {
+                }
+            }
+
+            foreach (var miniAc in d.MiniBadgeActivity)
+            {
+                if(dict.ContainsKey(miniAc))
+                {
+                    dict[miniAc].miniBadge = true;
+                }
+                else
+                {
+                    dict[miniAc] = new ActivityData
+                    {
+                        id = miniAc,
+                        badge = 0,
+                        miniBadge = true
+                    };
+                }
+            }
+
+            return dict.Values.ToList();
+        }
+    }
+}
---
+++ src/VessageRESTfulServer/Services/ActivityService.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
+using MongoDB.Driver;
+using MongoDB.Bson;
+
+namespace VessageRESTfulServer.Services
+{
+    public class ActivityBadgeData
+    {
+        public ObjectId Id { get; set; }
+        public ObjectId UserId { get; set; }
+        public string[] MiniBadgeActivity { get; set; }
+        public string[] BadgeValueActivity { get; set; }
+    }
+
+    public class ActivityService
+    {
+        protected IMongoClient Client { get; set; }
+        public ActivityService(IMongoClient Client)
+        {
+            this.Client = Client;
+        }
+
+        public async void AddActivityBadge(string activityId, string userId, int addiction)
+        {
+            var userOId = new ObjectId(userId);
+            var collection = Client.GetDatabase("ActivityBadgeData").GetCollection<ActivityBadgeData>("ActivityBadgeData");
+            try
+            {
+
+                var update = new UpdateDefinitionBuilder<ActivityBadgeData>().Push(d => d.BadgeValueActivity, string.Format("{0}:{1}", activityId, addiction));
+                var res = await collection.FindOneAndUpdateAsync(a => a.UserId == userOId, update);
+                if (res == null)
+                {
+                    var data = new ActivityBadgeData
+                    {
+                        BadgeValueActivity = new string[] { string.Format("{0}:{1}", activityId, addiction) },
+                        MiniBadgeActivity = new string[0],
+                        UserId = userOId
+                    };
+                    await collection.InsertOneAsync(data);
+                }
+            }
+            catch (Exception)
+            {
+                
+            }
+            
+        }
+
+        public async void SetActivityMiniBadge(string activityId, string userId)
+        {
+            var userOId = new ObjectId(userId);
+            var collection = Client.GetDatabase("ActivityBadgeData").GetCollection<ActivityBadgeData>("ActivityBadgeData");
+            try
+            {
+                var update = new UpdateDefinitionBuilder<ActivityBadgeData>().Push(d => d.MiniBadgeActivity, activityId);
+                var res = await collection.FindOneAndUpdateAsync(a => a.UserId == userOId, update);
+                if (res == null)
+                {
+                    var data = new ActivityBadgeData
+                    {
+                        BadgeValueActivity = new string[0],
+                        MiniBadgeActivity = new string[] { activityId },
+                        UserId = userOId
+                    };
+                    await collection.InsertOneAsync(data);
+                }
+            }
+            catch (Exception)
+            {
+                
+            }
+        }
+
+        public async Task<ActivityBadgeData> GetActivityBoardData(string userId)
+        {
+            var userOId = new ObjectId(userId);
+            var collection = Client.GetDatabase("ActivityBadgeData").GetCollection<ActivityBadgeData>("ActivityBadgeData");
+            var update1 = new UpdateDefinitionBuilder<ActivityBadgeData>().Set(d => d.BadgeValueActivity, new string[0]);
+            var update2 = new UpdateDefinitionBuilder<ActivityBadgeData>().Set(d => d.MiniBadgeActivity, new string[0]);
+            var update = new UpdateDefinitionBuilder<ActivityBadgeData>().Combine(update1, update2);
+            return await collection.FindOneAndUpdateAsync(a => a.UserId == userOId, update);
+        }
+    }
+
+    public static class ActivityServiceExtensions
+    {
+        public static ActivityService GetActivityService(this IServiceProvider provider)
+        {
+            return provider.GetService<ActivityService>();
+        }
+    }
+}
