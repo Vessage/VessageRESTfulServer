78cd90434cddac56f48d36c9bc92a3261fcc34d3
alexchow <cplover@live.com>
2016-12-25 00:21:58 +0800

    add mns

---
+++ src/VessageRESTfulServer/Activities/MYQ/MYQController.cs
@@ -0,0 +1,130 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using BahamutCommon;
+using MongoDB.Driver;
+using MongoDB.Bson;
+using VessageRESTfulServer.Services;
+using Microsoft.Extensions.Configuration;
+using System.IO;
+using VessageRESTfulServer.Controllers;
+using System.Net;
+
+namespace VessageRESTfulServer.Activities.MYQ
+{
+    public class MYQConfigCenter
+    {
+        public const string ActivityId = "1006";
+        private static IConfiguration _MYQConfig;
+        private static IConfiguration MYQConfig
+        {
+            get
+            {
+                if (_MYQConfig == null)
+                {
+                    var configRoot = Startup.ConfigRoot + Path.DirectorySeparatorChar;
+                    _MYQConfig = new ConfigurationBuilder()
+                        .AddJsonFile(string.Format("{0}myq_config.json", configRoot), true, true)
+                        .Build();
+                }
+                return _MYQConfig;
+            }
+        }
+    }
+
+    [Route("api/[controller]")]
+    public partial class MYQController : APIControllerBase
+    {
+        private IMongoDatabase MYQDb
+        {
+            get
+            {
+                var client = AppServiceProvider.GetSharedService().GetMongoDBClient();
+                return client.GetDatabase("MYQ");
+            }
+        }
+
+        [HttpGet("MainInfo")]
+        public async Task<object> GetMainInfo(string location = null)
+        {
+            var usrCol = MYQDb.GetCollection<MYQProfile>("MYQProfile");
+
+            var isNewer = false;
+            MYQProfile profile = null;
+            try
+            {
+                var filter = new FilterDefinitionBuilder<MYQProfile>().Where(f => f.UserId == UserObjectId);
+                var update = new UpdateDefinitionBuilder<MYQProfile>().Set(p => p.ActiveTime, DateTime.UtcNow);
+
+                if (!string.IsNullOrWhiteSpace(location))
+                {
+                    var c = Utils.LocationStringToLocation(location);
+                    update = update.Set(p => p.Location, c);
+                }
+                profile = await usrCol.FindOneAndUpdateAsync(filter, update);
+                if (profile == null || profile.Id == ObjectId.Empty)
+                {
+                    throw new NullReferenceException();
+                }
+            }
+            catch (NullReferenceException)
+            {
+                var myUserProfile = await AppServiceProvider.GetUserService().GetUserOfUserId(UserObjectId);
+                profile = new MYQProfile
+                {
+                    UserId = UserObjectId,
+                    ActiveTime = DateTime.UtcNow,
+                    CreateTime = DateTime.UtcNow,
+                    ProfileState = MYQProfile.STATE_NORMAL,
+                    Nick = myUserProfile.Nick,
+                    Avatar = myUserProfile.Avartar,
+                    Location = string.IsNullOrWhiteSpace(location) ? null : Utils.LocationStringToLocation(location)
+                };
+                await usrCol.InsertOneAsync(profile);
+                await AppServiceProvider.GetActivityService().CreateActivityBadgeData(MYQConfigCenter.ActivityId,UserObjectId);
+                isNewer = true;
+            }
+            IEnumerable<MYQProfile> profiles = await usrCol.Find(p =>p.ProfileState == MYQProfile.STATE_NORMAL).SortByDescending(p=>p.ActiveTime).Limit(100).ToListAsync();
+
+            return new
+            {
+                newer = isNewer,
+                ques = profile.Question,
+                usrQues = from p in profiles select MYQProfileToJsonObject(p)
+            };
+        }
+
+        [HttpPut("Question")]
+        public async Task<object> UpdateQuestionAsync(string ques)
+        {
+            var usrCol = MYQDb.GetCollection<MYQProfile>("MYQProfile");
+            var update = new UpdateDefinitionBuilder<MYQProfile>()
+            .Set(p => p.ActiveTime, DateTime.UtcNow)
+            .Set(p => p.Question, ques);
+            var r = await usrCol.UpdateOneAsync(f => f.UserId == UserObjectId, update);
+            if (r.ModifiedCount > 0 || r.MatchedCount > 0)
+            {
+                return new { msg = "SUCCESS" };
+            }
+            else
+            {
+                Response.StatusCode = (int)HttpStatusCode.NotModified;
+                return new { msg = "NOT_MODIFIED" };
+            }
+        }
+
+        private object MYQProfileToJsonObject(MYQProfile p)
+        {
+            return new
+            {
+                userId = p.UserId.ToString(),
+                nick = p.Nick,
+                ques = p.Question,
+                avatar = p.Avatar,
+                aTs = (long)DateTimeUtil.UnixTimeSpanOfDateTime(p.ActiveTime).TotalMilliseconds
+            };
+        }
+    }
+}
---
+++ src/VessageRESTfulServer/Activities/MYQ/Models.cs
@@ -0,0 +1,27 @@
+﻿using MongoDB.Bson;
+using MongoDB.Driver.GeoJsonObjectModel;
+using System;
+
+namespace VessageRESTfulServer.Activities.MYQ
+{
+    public class MYQProfile
+    {
+        public const int STATE_BLACK_LIST = -100;
+        public const int STATE_NORMAL = 1;
+        public ObjectId Id { get; set; }
+        public ObjectId UserId { get; set; }
+        
+        public string Nick { get; set; }
+
+        public string Avatar { get; set; }
+        public DateTime ActiveTime { get; set; }
+
+        public DateTime CreateTime { get; set; }
+
+        public string Question { get; set; }
+
+        public GeoJson2DGeographicCoordinates Location { get; set; }
+
+        public int ProfileState { get; set; }
+    }
+}
