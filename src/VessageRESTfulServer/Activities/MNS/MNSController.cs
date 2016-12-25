3bc31ce266dbe7047c46113d8d042d2cfe64c53f
alexchow <cplover@live.com>
2016-12-26 00:30:34 +0800

    update

--- src/VessageRESTfulServer/Activities/MNS/MNSController.cs
+++ src/VessageRESTfulServer/Activities/MNS/MNSController.cs
@@ -87,7 +87,7 @@ namespace VessageRESTfulServer.Activities.MNS
                 isNewer = true;
             }
             var now = DateTime.UtcNow;
-            var limit = now.AddHours(-1);
+            var limit = now.AddHours(-3);
             IEnumerable<MNSProfile> profiles = await usrCol.Find(p =>p.MidNightAnnounce != null && p.UserId != UserObjectId && p.ProfileState == MNSProfile.STATE_NORMAL && p.ActiveTime > limit).SortByDescending(p => p.ActiveTime).Limit(100).ToListAsync();
 
             return new
